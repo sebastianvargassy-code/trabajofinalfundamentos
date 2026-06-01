e723764d3c80264c929a126e789d4398fb37963b
sebastianvargassy-code <sebastianvargassy@gmail.com>
2026-06-01 11:00:57 -0500

    Update minimarket.py

--- minimarket.py
+++ minimarket.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template_string, redirect, url_for
 
 app = Flask(__name__)
-
+app.secret_key = 'clave'
 # INDEX REDISEÑADO (Solo modifiqué este bloque)
 index_html = """
 <!DOCTYPE html>
